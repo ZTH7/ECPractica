dcf3aec1f6737f25d595583ad8e7f1869434ba8b
itslubin <lubinyee@gmail.com>
2023-03-19 19:37:02 +0100

    segundo commit

--- Pr3.3/main.c
+++ Pr3.3/main.c
@@ -6,7 +6,9 @@ int main(void) {
     PM5CTL0 &= ~LOCKLPM5;
 
     P1DIR |= BIT0;
-
+    // TASSEL_1 se utiliza para seleccionar la fuente de reloj ACLK1 como la fuente de reloj del temporizador en MSP430.
+    // TACLR hace clear del temporizador
+    // MC_1 hace que el reloj cuente hacia arriba
     TA0CTL = TASSEL_1 | TACLR | MC_1;
     TA0CCR0 = 40000;
     TA0CCTL0 = CCIE; // Habilita interrupcion del temporizador (bit CCIE)
